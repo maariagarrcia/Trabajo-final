3a026d55129ec999e1acb39d5c6e821417df8b97
acasasaez <acasasae@myuax.com>
2022-05-12 16:09:04 +0200

    renombrar columnas

--- trabajo.py
+++ trabajo.py
@@ -2,3 +2,9 @@
 import pandas as pd 
 df_datos = pd.read_csv(r"output.csv") # Leemos el archivo csv
 #print(df_datos)
+#Traducimos los nombres de las columnas de nuestros datasets para facilitar el trabajo
+df_datos.rename(columns = {"Entity": "Entidad",
+"Code": "Identificador",
+"Year": "Año",
+"Deaths": "Muertes",
+"Sidness": "Lateralidad"})
